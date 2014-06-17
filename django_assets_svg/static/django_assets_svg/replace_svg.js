53de94e2d18045340b23df6a7dfacb5c13d6f56f
Chris Wilson <chris+github@qwirx.com>
2014-06-17 17:45:21 +0100

    Fix mistake in checking file extension in javascript.

--- django_assets_svg/static/django_assets_svg/replace_svg.js
+++ django_assets_svg/static/django_assets_svg/replace_svg.js
@@ -2,7 +2,7 @@ function replace_svg_logo_with_png()
 {
 	var current_url = this.src;
 	var png_url = current_url.substring(0, current_url.length - 4) + '.png';
-	if (current_url.substring(0, current_url.length - 4) == '.svg')
+	if (current_url.substring(current_url.length - 4) == '.svg')
 	{
 		this.src = png_url;
 	}
